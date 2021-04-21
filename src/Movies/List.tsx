94ef67dc860f399f4cf433e97dd4edc8ae0ece42
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2021-04-21 12:42:33 +0200

    Added pagination to the elements

--- src/Movies/List.tsx
+++ src/Movies/List.tsx
@@ -5,6 +5,7 @@ import MovieType from "../types/movie";
 import "./List.css";
 
 import {
+    Link,
     withRouter
 } from "react-router-dom";
 
@@ -24,24 +25,76 @@ function Movies(props: any){
 
     function paginateMovies() : MovieType[] {
         const { pageNumber } = props.match.params;
-        return movies.splice(DEFAULT_PER_PAGE * (pageNumber - 1), DEFAULT_PER_PAGE)
+        var moviesPage = [...movies]
+        return moviesPage.splice(DEFAULT_PER_PAGE * (parseInt(pageNumber)  - 1), DEFAULT_PER_PAGE)
+    }
+
+    function hasPreviousPage() : boolean {
+        const { pageNumber } = props.match.params;
+        console.log("haprev", parseInt(pageNumber) === 1)
+        if(parseInt(pageNumber) === 1){
+            return false;
+        }
+
+        return true;
+    }
+
+    function hasNextPage() : boolean {
+        const { pageNumber } = props.match.params;
+        console.log("parseInt(pageNumber) * DEFAULT_PER_PAGE",parseInt(pageNumber) * DEFAULT_PER_PAGE);
+        console.log("movies.length", movies.length)
+        if(parseInt(pageNumber) * DEFAULT_PER_PAGE < movies.length){
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    function previousPagePath() : string {
+        const { pageNumber } = props.match.params;
+
+        return "/movies/" + (parseInt(pageNumber) - 1);
+    }
+
+    
+    function nextPagePath() : string {
+        const { pageNumber } = props.match.params;
+
+        return "/movies/" + (parseInt(pageNumber) + 1);
     }
 
     return (
-        <section className="movies-list">
-            {
-                paginateMovies().map((movie: MovieType) => {
-                    return <Card 
-                        key={movie.id}
-                        id={movie.id}
-                        title={movie.title}
-                        category={movie.category}
-                        likes={movie.likes}
-                        dislikes={movie.dislikes}
-                    ></Card>
-                })
-            }
-        </section>
+        <>
+            <section className="movies-list">
+                {
+                    paginateMovies().map((movie: MovieType) => {
+                        return <Card 
+                            key={movie.id}
+                            id={movie.id}
+                            title={movie.title}
+                            category={movie.category}
+                            likes={movie.likes}
+                            dislikes={movie.dislikes}
+                        ></Card>
+                    })
+                }
+            </section>
+            <nav className="navigation-buttons">
+                {
+                    hasPreviousPage() &&
+                    <Link to={previousPagePath()} style={{float: "left", margin: "30px"}}>
+                        Previous
+                    </Link>
+                }
+                {
+                    hasNextPage() &&
+                    <Link to={nextPagePath()} style={{float: "right", margin: "30px"}}>
+                        Next
+                    </Link>
+                }
+            </nav>
+        </>
     )
 }
 
